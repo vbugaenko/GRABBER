2215b388875ca208918c41de53a385bf5f579a58
vbugaenko <vstrateg@yandex.ru>
2018-11-24 21:14:49 +1000

    update code (add de/serialization results of searching links by parser)

--- src/main/java/ru/grabber/Main.java
+++ src/main/java/ru/grabber/Main.java
@@ -1,5 +1,8 @@
 package ru.grabber;
 
+import org.apache.log4j.Logger;
+
+import java.io.*;
 import java.net.URI;
 import java.util.Set;
 
@@ -11,12 +14,22 @@ import java.util.Set;
  */
 
 public class Main {
+  private static final org.apache.log4j.Logger logger = Logger.getLogger(Main.class);
   private static final String WEBSITE = "";
 
   public static void main(String[] args)  {
+    //Распарсить сайт по ссылкам
     Set<URI> uris = new Parse(WEBSITE).getURIs();
-    System.out.println("Ссылок всего "+uris.size());
-    new Grab ( uris , folder(WEBSITE) );
+    String project = folder(WEBSITE);
+    File file = new File(project+".ser");
+    serializeObject(file.getName(), uris);
+
+    //Граб
+    Set<URI> urisLoaded = readObject(file.getName());
+    if (urisLoaded != null) {
+      logger.info(urisLoaded.size() + " links prepared for loading from web");
+      new Grab(uris, project);
+    }
   }
 
   /**
@@ -30,5 +43,27 @@ public class Main {
             .replaceAll(".*(www.)","")
             .replaceAll("(.ru).*", "");
   }
+
+  private static Set<URI> readObject(String fileName)  {
+    Set<URI> uris = null;
+    try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName)) ) {
+        Object object = inputStream.readObject();
+        uris = (Set<URI>)object;
+        logger.info("data loaded from ["+ fileName +"]");
+    } catch (Exception e) {
+      logger.error("Problem with deserialization from (" + fileName + ") file: " + e.getMessage());
+    }
+    return uris;
+  }
+
+  private static void serializeObject(String filename, Set<URI> storage) {
+    try (ObjectOutputStream bOutputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
+      bOutputStream.writeObject(storage);
+      logger.info("data saved to ["+ filename +"]");
+    } catch (Exception e) {
+      logger.error("Problem with serialization in (" + filename + ") file: " + e.getMessage());
+    }
+  }
+
 
 }
